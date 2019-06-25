c380eea34b5d165b3bc182f059db2deec30fbbeb
Colin <beutt102@mail.chapman.edu>
2019-06-24 22:30:57 -0700

    update

--- public/parallax.js
+++ public/parallax.js
@@ -6,7 +6,7 @@ function parallax(){
    
     var imgPos =$(window).scrollTop();
     
-    $(".parallax").css("transform", 'translateY(0px,-' + imgPos/2+'%)');
+    $(".header").css("transform", 'translateX(0px,-' + imgPos/2+'%)');
 
 }
 
